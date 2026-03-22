f0e31696751645f838038cc268f81cc4f7813f0d
Tochukwu-d <tochukwutony1924@gmail.com>
2026-03-22 18:36:59 +0100

    groupping by different cell types to get granular information

--- Morphological_feature_comparison.sql
+++ Morphological_feature_comparison.sql
@@ -96,3 +96,57 @@ Across all six morphological features, the average values are consistently highe
 implying a low coefficient of variation. This suggests that the data exhibits limited dispersion relative to 
 its mean, indicating homogeneity in cell characteristics within each cell type.
 */
+
+-- deeper statistically dive
+SELECT
+    cell_type,
+    anomaly_label,
+    ROUND(
+        AVG(cell_diameter_um), 4
+    ) AS avg_cell_diameter,
+
+    ROUND(
+        STDDEV(cell_diameter_um), 4
+    ) AS stddev_cell_diameter,
+
+    ROUND(
+        AVG(cell_area_px), 4
+    ) AS avg_cell_area,
+
+    ROUND(
+        STDDEV(cell_area_px), 4
+    ) AS stddev_cell_area,
+
+    ROUND(
+        AVG(circularity), 4
+    ) AS avg_circularity,
+
+    ROUND(
+        STDDEV(circularity), 4
+    ) AS stddev_circularity,
+
+    ROUND(
+        AVG(eccentricity), 4
+    ) AS avg_eccentricity,
+
+    ROUND(
+        STDDEV(eccentricity), 4
+    ) AS stddev_eccentricity,
+
+    ROUND(
+        AVG(lobularity_score), 4
+    ) AS avg_lobularity_score,
+
+    ROUND(
+        STDDEV(lobularity_score), 4
+    ) AS stddev_lobularity_score,
+
+    ROUND(
+        AVG(membrane_smoothness), 4
+    ) AS avg_membrane_smoothness,
+
+    ROUND(
+        STDDEV(membrane_smoothness), 4
+    ) AS stddev_membrane_smoothness
+FROM blood_cell_anomaly_detection
+GROUP BY cell_type, anomaly_label;
